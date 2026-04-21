ce3c11aee55ee0df3bbf230d582a4d615c3e725d
Luc Allaire <wolf-361@hotmail.com>
2026-04-21 10:36:08 -0400

    feat(app): wire up full-height shell with routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { NavbarComponent } from './core/layout/navbar/navbar';
+import { FooterComponent } from './core/layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  template: `
+    <app-navbar />
+    <main>
+      <router-outlet />
+    </main>
+    <app-footer />
+  `,
+  styles: [
+    `
+      :host {
+        display: flex;
+        flex-direction: column;
+        min-height: 100dvh;
+      }
+      main {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+      }
+    `,
+  ],
 })
-export class App {
-  protected readonly title = signal('angular-21-standalone-template');
-}
+export class App {}
